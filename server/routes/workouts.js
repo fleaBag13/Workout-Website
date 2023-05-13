9d9d5bfb9ef8016afe10537c62e7223d35ba858c
fleaBag13 <dyutika.khatate@gmail.com>
2023-05-13 08:53:03 +0530

    Workout website created

---
+++ server/routes/workouts.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { createWorkout, getWorkout, getWorkouts, deleteWorkout, updateWorkout } from "../controllers/workoutController.js";
+
+const router = express.Router();
+
+router.get('/', getWorkouts);
+
+router.get('/:id', getWorkout);
+
+router.post('/', createWorkout);
+
+router.delete('/:id', deleteWorkout);
+
+router.patch('/:id', updateWorkout);
+
+export default router;
